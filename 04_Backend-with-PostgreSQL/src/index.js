abc706fbffa4fcd4f4b59cc209e4f7787d58d85c
Hemant Sah <hemantsah2912@gmail.com>
2026-02-20 20:55:00 +0530

    have use latest prisma version

---
+++ 04_Backend-with-PostgreSQL/src/index.js
@@ -0,0 +1,10 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`app is listening on PORT: ${process.env.PORT}`)
+})
