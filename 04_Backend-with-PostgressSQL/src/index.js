f78334e96c8f37072f64703756abbc1681d9c12c
Hemant Sah <hemantsah2912@gmail.com>
2026-02-23 03:57:11 +0530

    understood postgress set-up

--- 04_Backend-with-PostgressSQL/src/index.js
+++ 04_Backend-with-PostgressSQL/src/index.js
@@ -1,9 +1,15 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import authRouter from "./routes/auth.routes.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 9995;
+
+
+app.use("/api/v1/users", authRouter);
+
+
 
 app.listen(PORT, ()=>{
     console.log(`app is listening on PORT: ${PORT}`);
