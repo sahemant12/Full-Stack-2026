3e60ccedcc167714a9d5931e5bcdd030a991c53c
Hemant Sah <hemantsah2912@gmail.com>
2026-02-25 23:30:22 +0530

    add problem controller and routes

---
+++ 08-Leet_Lab/src/controllers/problem.controller.js
@@ -0,0 +1,7 @@
+
+export const createProblem = async (req, res)=>{};
+export const getAllProblems = async (req, res)=>{};
+export const getProblemById = async (req, res)=>{};
+export const updateProblem = async (req, res)=>{};
+export const deleteProblem = async (req, res)=>{};
+export const getAllProblemsSolvedByUser = async (req, res)=>{};
