af1aef85cbd3952cc2390a5748c245cf517b0f73
Hemant Sah <hemantsah2912@gmail.com>
2026-02-28 23:24:24 +0530

    updateProblem done

--- 08-Leet_Lab/src/controllers/problem.controller.js
+++ 08-Leet_Lab/src/controllers/problem.controller.js
@@ -79,6 +79,8 @@ export const getAllProblems = async (req, res)=>{
     // get all problems
 };
 export const getProblemById = async (req, res)=>{};
-export const updateProblem = async (req, res)=>{};
+export const updateProblem = async (req, res)=>{
+    // updateproblem
+};
 export const deleteProblem = async (req, res)=>{};
 export const getAllProblemsSolvedByUser = async (req, res)=>{};
