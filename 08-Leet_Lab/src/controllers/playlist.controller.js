bedc7cc23f70d5902794a0501b5270b93f75d5bb
Hemant Sah <hemantsah2912@gmail.com>
2026-03-05 23:37:54 +0530

    add playlist controller method

---
+++ 08-Leet_Lab/src/controllers/playlist.controller.js
@@ -0,0 +1,7 @@
+
+export const createPlayList = async (req, res)=>{};
+export const getPlayAllListDetails = async (req, res)=>{};
+export const getPlayListDetails = async (req, res)=>{};
+export const addProblemToPlaylist = async (req, res)=>{};
+export const deletePlayList = async (req, res)=>{};
+export const removeProblemFromPlaylist = async (req, res)=>{};
