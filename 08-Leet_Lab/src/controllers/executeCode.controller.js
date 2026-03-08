e3f5caba1453fcfc5695304516ac5928c9f15db8
Hemant Sah <hemantsah2912@gmail.com>
2026-03-08 09:46:38 +0530

    add executeCode Controller

--- 08-Leet_Lab/src/controllers/executeCode.controller.js
+++ 08-Leet_Lab/src/controllers/executeCode.controller.js
@@ -1,9 +1,23 @@
 
 export const executeCode = async (req, res) =>{
-    const {problemId, language_id, source_code, stdin, expected_outputs, } = req.body;
-    const userId = req.user.id;
     try {
-        
+        const {problemId, language_id, source_code, stdin, expected_outputs } = req.body;
+
+        // validate data
+        if(!Array.isArray(stdin) || !Array.isArray(expected_outputs) || stdin.length===0 || stdin.length!==expected_outputs.length){
+            return res.status(400).json({
+                message: "Invalid data"
+            });
+        }
+
+        // prepare submissions for each test cases:
+        const submissions = stdin.map((input)=>{
+       source_code: solutionCode,
+        language_id: languageId,
+        stdin: input,
+        expected_output: output,            
+        });
+
     } catch (error) {
         
     }
