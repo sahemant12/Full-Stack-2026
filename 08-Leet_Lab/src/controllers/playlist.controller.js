07eebbefb17a377de4313d419b71aefe445e1ba2
Hemant Sah <hemantsah2912@gmail.com>
2026-03-09 14:35:39 +0530

    add getPlayListDetails

--- 08-Leet_Lab/src/controllers/playlist.controller.js
+++ 08-Leet_Lab/src/controllers/playlist.controller.js
@@ -5,7 +5,9 @@ export const createPlayList = async (req, res)=>{
 export const getPlayAllListDetails = async (req, res)=>{
     // get playlist details
 };
-export const getPlayListDetails = async (req, res)=>{};
+export const getPlayListDetails = async (req, res)=>{
+    // getPlayListDetails
+};
 export const addProblemToPlaylist = async (req, res)=>{};
 export const deletePlayList = async (req, res)=>{};
 export const removeProblemFromPlaylist = async (req, res)=>{};
